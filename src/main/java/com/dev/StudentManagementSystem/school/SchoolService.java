8e0d842ea153c488529ae7d383ec54b5f63c1ea5
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-04 16:12:42 +0000

    feat: implemented update and delete calls, and added comments

--- src/main/java/com/dev/StudentManagementSystem/school/SchoolService.java
+++ src/main/java/com/dev/StudentManagementSystem/school/SchoolService.java
@@ -1,7 +1,9 @@
 package com.dev.StudentManagementSystem.school;
 
+import com.dev.StudentManagementSystem.common.ResourceNotFoundException;
 import com.dev.StudentManagementSystem.school.dto.CreateSchoolRequest;
 import com.dev.StudentManagementSystem.school.dto.SchoolResponse;
+import com.dev.StudentManagementSystem.school.dto.UpdatedSchoolResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,9 @@ public class SchoolService {
 
     private final SchoolRepository schoolRepository;
 
+    /**
+     * Create new school entity
+     */
     public SchoolResponse createSchool(CreateSchoolRequest request) {
 
         if (schoolRepository.existsByName(request.getName())) {
@@ -31,6 +36,10 @@ public class SchoolService {
         return toResponse(saved);
     }
 
+    /**
+     * Get all schools both active and inactive
+     */
+    @Transactional(readOnly = true)
     public List<SchoolResponse> getAllSchools() {
 
         return schoolRepository.findAll()
@@ -39,6 +48,47 @@ public class SchoolService {
                 .toList();
     }
 
+    /**
+     * Update School information
+     */
+    public UpdatedSchoolResponse updateSchoolInfo(Long id, CreateSchoolRequest request) {
+
+        School school = schoolRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("School not found with id: " + id));
+
+        if (!school.getName().equals(request.getName()) && schoolRepository.existsByName(request.getName())) {
+            throw new IllegalStateException("School already exits with name: " + request.getName());
+        }
+
+        school.setName(request.getName());
+
+        return toUpdateResponse(school);
+    }
+
+    /**
+     * Deactivate School
+     */
+    public void deActivateSchool(Long id) {
+
+        School school = schoolRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("School not found with id: " + id));
+
+        school.setIsActive(false);
+    }
+
+    /**
+     * Method helper to find school by id
+     */
+    @Transactional(readOnly = true)
+    public SchoolResponse findById(Long id) {
+        School school = schoolRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("School not found with id: " + id));
+        return toResponse(school);
+    }
+
+    /**
+     * Map type Student to type StudentResponse
+     */
     private SchoolResponse toResponse(School saved) {
         SchoolResponse response = new SchoolResponse();
 
@@ -46,6 +96,22 @@ public class SchoolService {
         response.setName(saved.getName());
         response.setIsActive(saved.getIsActive());
         response.setCreatedAt(saved.getCreatedAt());
+
+        return response;
+    }
+
+    /**
+     * Map type School to type UpdatedSchoolResponse
+     */
+    private UpdatedSchoolResponse toUpdateResponse(School saved) {
+
+        UpdatedSchoolResponse response = new UpdatedSchoolResponse();
+
+        response.setId(saved.getId());
+        response.setName(saved.getName());
+        response.setIsActive(saved.getIsActive());
+        response.setCreatedAt(saved.getCreatedAt());
+        response.setUpdatedAt(saved.getUpdatedAt());
 
         return response;
     }
