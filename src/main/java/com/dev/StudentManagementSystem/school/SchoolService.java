9a8f8db7105ef347d9f05fce15f735ba43fba896
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-04 12:55:57 +0000

    feat: created service for school

---
+++ src/main/java/com/dev/StudentManagementSystem/school/SchoolService.java
@@ -0,0 +1,40 @@
+package com.dev.StudentManagementSystem.school;
+
+import com.dev.StudentManagementSystem.school.dto.CreateSchoolRequest;
+import com.dev.StudentManagementSystem.school.dto.SchoolResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Transactional
+@Service
+public class SchoolService {
+
+    private final SchoolRepository schoolRepository;
+
+    public SchoolResponse createSchool(CreateSchoolRequest request) {
+
+        if (schoolRepository.existsByName(request.getName())) {
+            throw new IllegalStateException("A school exist with the name: " + request.getName());
+        }
+
+        School school = new School();
+        school.setName(request.getName());
+
+
+        School saved = schoolRepository.save(school);
+        return toResponse(saved);
+    }
+
+    private SchoolResponse toResponse(School saved) {
+        SchoolResponse response = new SchoolResponse();
+
+        response.setId(saved.getId());
+        response.setName(saved.getName());
+        response.setIsActive(saved.getIsActive());
+        response.setCreatedAt(saved.getCreatedAt());
+
+        return response;
+    }
+}
