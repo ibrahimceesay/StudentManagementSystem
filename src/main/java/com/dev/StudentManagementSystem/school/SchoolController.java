7118eb756bdce980ff33605c1b2ddb6c45c3b540
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-04 16:16:08 +0000

    feat: implemented update and delete calls and added comments

--- src/main/java/com/dev/StudentManagementSystem/school/SchoolController.java
+++ src/main/java/com/dev/StudentManagementSystem/school/SchoolController.java
@@ -2,8 +2,10 @@ package com.dev.StudentManagementSystem.school;
 
 import com.dev.StudentManagementSystem.school.dto.CreateSchoolRequest;
 import com.dev.StudentManagementSystem.school.dto.SchoolResponse;
+import com.dev.StudentManagementSystem.school.dto.UpdatedSchoolResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +18,48 @@ public class SchoolController {
 
     private final SchoolService schoolService;
 
+    /**
+     * Post mapping to create a new school entity
+     */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public SchoolResponse createSchool(@Valid @RequestBody CreateSchoolRequest request) {
         return schoolService.createSchool(request);
     }
 
+    /**
+     * Get mapping to retrieve all schools
+     */
     @GetMapping
-    public List<SchoolResponse> getAllSchools(){
+    @ResponseStatus(HttpStatus.OK)
+    public List<SchoolResponse> getAllSchools() {
         return schoolService.getAllSchools();
     }
 
+    /**
+     * Get mapping to find a school by id
+     */
     @GetMapping("/{id}")
-    public SchoolResponse findById(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.OK)
+    public SchoolResponse findById(@PathVariable Long id) {
         return schoolService.findById(id);
+    }
+
+    /**
+     * Patch mapping to update school information with id
+     */
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UpdatedSchoolResponse updateSchool(@PathVariable Long id, @Valid @RequestBody CreateSchoolRequest request) {
+        return schoolService.updateSchoolInfo(id, request);
+    }
+
+    /**
+     * Delete mapping to deactivate school with id
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deActivateSchool(@PathVariable Long id) {
+        schoolService.deActivateSchool(id);
     }
 }
