89c15a78a97ecb66c35eb0c69cbf73fbead2bac0
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-04 18:53:06 +0000

    chore: updated repository

--- src/main/java/com/dev/StudentManagementSystem/department/DepartmentRepository.java
+++ src/main/java/com/dev/StudentManagementSystem/department/DepartmentRepository.java
@@ -1,11 +1,16 @@
 package com.dev.StudentManagementSystem.department;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
-    Optional<Department> findById(Long id);
+    Boolean existsByName(String name);
+
+    Boolean existsByDepartmentCode(String departmentCode);
 
 }
