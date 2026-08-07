495c54aa7f49bd49c8de3cc872114c21817d6c65
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-07 15:42:41 +0000

    feat: implemented DTO for updatedLecturer response

---
+++ src/main/java/com/dev/StudentManagementSystem/lecturer/dto/UpdateLecturerRequest.java
@@ -0,0 +1,24 @@
+package com.dev.StudentManagementSystem.lecturer.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateLecturerRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is invalid")
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    @NotNull(message = "Department id is required")
+    private Long departmentId;
+}
