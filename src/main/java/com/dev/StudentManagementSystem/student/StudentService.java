b7dc2e40243eb2304fbcf10ec1e749c8aa443526
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-09 13:23:05 +0000

    feat: implemented services to getStudents, getStudent, updateStudent, deActivateStudent, findById, convert to UpdatedStudentResponse

--- src/main/java/com/dev/StudentManagementSystem/student/StudentService.java
+++ src/main/java/com/dev/StudentManagementSystem/student/StudentService.java
@@ -2,32 +2,38 @@ package com.dev.StudentManagementSystem.student;
 
 import com.dev.StudentManagementSystem.common.ResourceNotFoundException;
 import com.dev.StudentManagementSystem.department.Department;
-import com.dev.StudentManagementSystem.department.DepartmentRepository;
+import com.dev.StudentManagementSystem.department.DepartmentService;
+import com.dev.StudentManagementSystem.student.dto.CreateStudentRequest;
+import com.dev.StudentManagementSystem.student.dto.StudentResponse;
+import com.dev.StudentManagementSystem.student.dto.UpdateStudentRequest;
+import com.dev.StudentManagementSystem.student.dto.UpdatedStudentResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.dev.StudentManagementSystem.student.dto.CreateStudentRequest;
-import com.dev.StudentManagementSystem.student.dto.StudentResponse;
 
 import java.time.Year;
+import java.util.List;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class StudentService {
 
-    private final DepartmentRepository departmentRepository;
     private final StudentRepository studentRepository;
+    private final DepartmentService departmentService;
 
-    public StudentResponse createStudent(CreateStudentRequest request){
+    /**
+     * Create student service
+     */
+    public StudentResponse createStudent(CreateStudentRequest request) {
 
-        Department department = findDepartment(request.getDepartmentId());
+        Department department = departmentService.findById(request.getDepartmentId());
 
-        if (studentRepository.existsByEmail(request.getEmail())){
+        if (studentRepository.existsByEmail(request.getEmail())) {
             throw new IllegalStateException("A student already exits with the email: " + request.getEmail());
         }
 
-        if (studentRepository.existsByPhoneNumber(request.getPhoneNumber())){
+        if (studentRepository.existsByPhoneNumber(request.getPhoneNumber())) {
             throw new IllegalStateException("This phone number is already in use");
         }
 
@@ -46,6 +52,77 @@ public class StudentService {
         return toResponse(saved);
     }
 
+    /**
+     * Retrieve all students
+     */
+    @Transactional(readOnly = true)
+    public List<StudentResponse> getStudents() {
+        return studentRepository.findAll()
+                .stream()
+                .map(this::toResponse)
+                .toList();
+    }
+
+    /**
+     * Retrieve a student by id
+     */
+    @Transactional(readOnly = true)
+    public StudentResponse getStudent(Long id) {
+
+        Student student = findById(id);
+
+        return toResponse(student);
+    }
+
+    /**
+     * Update student service
+     */
+    public UpdatedStudentResponse updateStudent(Long id, UpdateStudentRequest request) {
+
+        Student student = findById(id);
+        Department department = departmentService.findById(request.getDepartmentId());
+
+        if (!student.getPhoneNumber().equals(request.getPhoneNumber()) && studentRepository.existsByPhoneNumber(request.getPhoneNumber())) {
+            throw new IllegalStateException("Student exist with phone number: " + request.getPhoneNumber());
+        }
+
+        if (!student.getEmail().equals(request.getEmail()) && studentRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalStateException("Student exist with email: " + request.getEmail());
+        }
+
+        student.setName(request.getName());
+        student.setPhoneNumber(request.getPhoneNumber());
+        student.setEmail(request.getEmail());
+        student.setGender(request.getGender());
+        student.setDepartment(department);
+
+        Student saved = studentRepository.save(student);
+
+        return toUpdatedStudentResponse(saved);
+    }
+
+    /**
+     * Deactivate student service
+     */
+    public void deActivateStudent(Long id) {
+
+        Student student = findById(id);
+
+        student.setIsActive(false);
+        studentRepository.save(student);
+    }
+
+    /**
+     * Helper method to find a student by id
+     */
+    public Student findById(Long id) {
+        return studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found with id: " + id));
+    }
+
+    /**
+     * Helper method to cast Student to StudentResponse
+     */
     private StudentResponse toResponse(Student saved) {
 //        return new StudentResponse(saved.getName(), saved.getPhoneNumber(),saved.getEmail(), saved.getGender(), saved.getStudentNumber(), saved.getDepartment().getName());
 
@@ -64,7 +141,30 @@ public class StudentService {
         return studentResponse;
     }
 
+    /**
+     * Helper method to cast Student to UpdatedStudentResponse
+     */
+    private UpdatedStudentResponse toUpdatedStudentResponse(Student saved) {
 
+        UpdatedStudentResponse studentResponse = new UpdatedStudentResponse();
+
+        studentResponse.setId(saved.getId());
+        studentResponse.setName(saved.getName());
+        studentResponse.setPhoneNumber(saved.getPhoneNumber());
+        studentResponse.setEmail(saved.getEmail());
+        studentResponse.setGender(saved.getGender());
+        studentResponse.setStudentNumber(saved.getStudentNumber());
+        studentResponse.setSchoolName(saved.getDepartment().getSchool().getName());
+        studentResponse.setDepartmentName(saved.getDepartment().getName());
+        studentResponse.setCreatedAt(saved.getCreatedAt());
+        studentResponse.setUpdatedAt(saved.getUpdatedAt());
+
+        return studentResponse;
+    }
+
+    /**
+     * Helper method to generate an alphanumerical studentNumber
+     */
     private String generateStudentNumber(Department department) {
 
         String year = Integer.toString(Year.now().getValue());
@@ -77,10 +177,5 @@ public class StudentService {
         String paddedCount = String.format("%03d", count);
 
         return prefix + paddedCount;
-    }
-
-    private Department findDepartment(Long id) {
-        return departmentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Department not found with id: " + id));
     }
 }
