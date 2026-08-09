4e534de25c13ccd93d0a89de9885b0e02b68588a
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-09 13:16:07 +0000

    feat: implemented calls to getCourse, getAllCoursesByDepartment, updateCourse, and deActivate course

--- src/main/java/com/dev/StudentManagementSystem/course/CourseController.java
+++ src/main/java/com/dev/StudentManagementSystem/course/CourseController.java
@@ -2,10 +2,14 @@ package com.dev.StudentManagementSystem.course;
 
 import com.dev.StudentManagementSystem.course.dto.CourseResponse;
 import com.dev.StudentManagementSystem.course.dto.CreateCourseRequest;
+import com.dev.StudentManagementSystem.course.dto.UpdateCourseRequest;
+import com.dev.StudentManagementSystem.course.dto.UpdateCourseResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 
@@ -19,5 +23,29 @@ public class CourseController {
     @ResponseStatus(HttpStatus.CREATED)
     public CourseResponse createCourse(@Valid @RequestBody CreateCourseRequest request){
         return courseService.createCourse(request);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CourseResponse getCourse(@PathVariable Long id){
+        return courseService.getCourse(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CourseResponse> getAllCourses(){
+        return courseService.getAllCourses();
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UpdateCourseResponse updateCourse(@PathVariable Long id, @Valid @RequestBody UpdateCourseRequest request){
+        return courseService.updateCourse(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deActivateCourse(@PathVariable Long id){
+        courseService.deActivateCourse(id);
     }
 }
