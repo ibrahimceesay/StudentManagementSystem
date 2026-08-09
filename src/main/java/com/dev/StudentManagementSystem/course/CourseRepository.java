6be1186fb06d4c0095770bc4ac3f22c52d83526e
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-09 13:16:35 +0000

    feat: implemented findByDepartmentId

--- src/main/java/com/dev/StudentManagementSystem/course/CourseRepository.java
+++ src/main/java/com/dev/StudentManagementSystem/course/CourseRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     List<Course> findByLecturerId(Long LecturerId);
+
+    List<Course> findByDepartmentId(Long DepartmentId);
 }
