24ef804bc8afc5b4e298a40d8632bc14543c4f0e
ibrahimceesay <ibraceesay981@gmail.com>
2026-08-14 20:54:57 +0000

    refactor: added comments

--- src/main/java/com/dev/StudentManagementSystem/course/CourseController.java
+++ src/main/java/com/dev/StudentManagementSystem/course/CourseController.java
@@ -19,48 +19,66 @@ public class CourseController {
 
     private final CourseService courseService;
 
+    /**
+     * Create new course
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CourseResponse createCourse(@Valid @RequestBody CreateCourseRequest request){
+    public CourseResponse createCourse(@Valid @RequestBody CreateCourseRequest request) {
         return courseService.createCourse(request);
     }
 
+    /**
+     * Get course by id
+     */
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public CourseResponse getCourse(@PathVariable Long id){
+    public CourseResponse getCourse(@PathVariable Long id) {
         return courseService.getCourse(id);
     }
 
+    /**
+     * get All courses
+     */
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<CourseResponse> getAllCourses(){
+    public List<CourseResponse> getAllCourses() {
         return courseService.getAllCourses();
     }
 
     /**
      * Retrieve all courses from a department
-     * */
+     */
     @GetMapping("/department/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public List<CourseResponse> getCoursesByDepartment(@PathVariable Long id){
+    public List<CourseResponse> getCoursesByDepartment(@PathVariable Long id) {
         return courseService.getCoursesByDepartmentId(id);
     }
 
+    /**
+     * Get all courses assigned to a lecturer by id
+     */
     @GetMapping("/lecturer/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public List<CourseResponse> getLecturerCourses(@PathVariable Long id){
+    public List<CourseResponse> getLecturerCourses(@PathVariable Long id) {
         return courseService.getCoursesByLecturerId(id);
     }
 
+    /**
+     * Update course info
+     */
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public UpdateCourseResponse updateCourse(@PathVariable Long id, @Valid @RequestBody UpdateCourseRequest request){
+    public UpdateCourseResponse updateCourse(@PathVariable Long id, @Valid @RequestBody UpdateCourseRequest request) {
         return courseService.updateCourse(id, request);
     }
 
+    /**
+     * Deactivate course
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deActivateCourse(@PathVariable Long id){
+    public void deActivateCourse(@PathVariable Long id) {
         courseService.deActivateCourse(id);
     }
 }
